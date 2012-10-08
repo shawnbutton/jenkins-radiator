486b45d9a4c3f7e06dadb9887611ebddd2b3f88f
kogitant <kogitant@koant.com>
2012-10-08 07:34:27 +0300

    Added tests for all job state methods

--- src/test/js/model/jobtests.js
+++ src/test/js/model/jobtests.js
@@ -8,6 +8,8 @@ test("Job Abdera-trunk from json is valid", function(){
     var job = new JR.Job(json.jobs[0]);
     ok(job);
     equal(job.getName(), 'Abdera-trunk');
+    equal(job.getColor(), "aborted");
+    ok(job.isAborted());
 });
 
 test("Job Abdera2-trunk from json is valid", function(){
@@ -15,6 +17,9 @@ test("Job Abdera2-trunk from json is valid", function(){
     ok(job);
     ok(job.isValid());
     equal(job.getName(), 'Abdera2-trunk');
+    equal(job.getColor(), "red_anime");
+    ok(job.isFailing());
+    ok(job.isBuilding());
 });
 
 test("Job Accumulo-1.3.x from json is valid", function(){
@@ -22,6 +27,8 @@ test("Job Accumulo-1.3.x from json is valid", function(){
     ok(job);
     ok(job.isValid());
     equal(job.getName(), 'Accumulo-1.3.x');
+    equal(job.getColor(), "disabled");
+    ok(job.isDisabled());
 });
 
 test("Job Accumulo-1.4.x from json is valid", function(){
@@ -29,6 +36,8 @@ test("Job Accumulo-1.4.x from json is valid", function(){
     ok(job);
     ok(job.isValid());
     equal(job.getName(), 'Accumulo-1.4.x');
+    equal(job.getColor(), "blue");
+    ok(job.isPassing());
 });
 
 test("Job Accumulo-trunk from json is valid", function(){
@@ -36,6 +45,8 @@ test("Job Accumulo-trunk from json is valid", function(){
     ok(job);
     ok(job.isValid());
     equal(job.getName(), 'Accumulo-Trunk');
+    equal(job.getColor(), "blue");
+    ok(job.isPassing());
 });
 
 test("Job ACE-trunk from json is valid", function(){
@@ -43,6 +54,8 @@ test("Job ACE-trunk from json is valid", function(){
     ok(job);
     ok(job.isValid());
     equal(job.getName(), 'ACE-trunk');
+    equal(job.getColor(), "yellow");
+    ok(job.isUnstable());
 });
 
 test("Job ActiveMQ from json is valid", function(){
@@ -50,7 +63,20 @@ test("Job ActiveMQ from json is valid", function(){
     ok(job);
     ok(job.isValid());
     equal(job.getName(), 'ActiveMQ');
+    equal(job.getColor(), "red");
+    ok(job.isFailing());
 });
+
+test("Job ZooKeeper_branch34_solaris from json is valid", function(){
+    var job = new JR.Job(json.jobs[7]);
+    ok(job);
+    ok(job.isValid());
+    equal(job.getName(), 'ZooKeeper_branch34_solaris');
+    equal(job.getColor(), "blue_anime");
+    ok(job.isPassing());
+    ok(job.isBuilding());
+});
+
 
 test("Job with unknown color is not valid", function(){
     try{
